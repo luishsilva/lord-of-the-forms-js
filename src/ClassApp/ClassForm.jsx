73e10e869e63114f58986a385f61e46551342d08
luishsilva <luizhrick@gmail.com>
2024-01-26 07:03:33 -0700

    Create handleChange setState method html input with InputText component

--- src/ClassApp/ClassForm.jsx
+++ src/ClassApp/ClassForm.jsx
@@ -1,5 +1,6 @@
 import { Component } from "react";
 import { ErrorMessage } from "../ErrorMessage";
+import { TextInput } from "./components/TextInput";
 
 const firstNameErrorMessage = "First name must be at least 2 characters long";
 const lastNameErrorMessage = "Last name must be at least 2 characters long";
@@ -8,39 +9,65 @@ const cityErrorMessage = "State is Invalid";
 const phoneNumberErrorMessage = "Invalid Phone Number";
 
 export class ClassForm extends Component {
+  handleChange = (e) => {
+    const { name, value } = e.target;
+    this.props.setStateMethod({ [name]: value });
+  };
+
   render() {
+    const { onSubmit } = this.props;
+    const { city, email, firstName, lastName, phone } = this.props.stateData;
     return (
-      <form>
+      <form onSubmit={onSubmit}>
         <u>
           <h3>User Information Form</h3>
         </u>
-
         {/* first name input */}
-        <div className="input-wrap">
-          <label>{"First Name"}:</label>
-          <input placeholder="Bilbo" />
-        </div>
+        <TextInput 
+          label="First Name"
+          value={firstName}
+          inputProps={{
+            placeholder:"First Name",
+            name: "firstName",
+            onChange: this.handleChange
+          }}
+        />
         <ErrorMessage message={firstNameErrorMessage} show={true} />
 
         {/* last name input */}
-        <div className="input-wrap">
-          <label>{"Last Name"}:</label>
-          <input placeholder="Baggins" />
-        </div>
+        <TextInput 
+          label="Last Name"
+          value={lastName}
+          inputProps={{
+            placeholder:"Last Name",
+            name: "lastName",
+            onChange: this.handleChange}
+          }
+        />
         <ErrorMessage message={lastNameErrorMessage} show={true} />
 
         {/* Email Input */}
-        <div className="input-wrap">
-          <label>{"Email"}:</label>
-          <input placeholder="bilbo-baggins@adventurehobbits.net" />
-        </div>
+        <TextInput 
+          label="Email"
+          value={email}
+          inputProps={{
+            placeholder:"Email",
+            name: "email",
+            onChange: this.handleChange}
+          }
+        />
         <ErrorMessage message={emailErrorMessage} show={true} />
 
         {/* City Input */}
-        <div className="input-wrap">
-          <label>{"City"}:</label>
-          <input placeholder="Hobbiton" />
-        </div>
+        <TextInput 
+          label="City"
+          value={city}
+          inputProps={{
+            placeholder:"Hobbiton",
+            name: "city",
+            onChange: this.handleChange}
+          }
+        />
         <ErrorMessage message={cityErrorMessage} show={true} />
 
         <div className="input-wrap">
@@ -58,7 +85,10 @@ export class ClassForm extends Component {
 
         <ErrorMessage message={phoneNumberErrorMessage} show={true} />
 
-        <input type="submit" value="Submit" />
+        <input 
+          type="submit" 
+          value="Submit" 
+        />
       </form>
     );
   }
