6a9ab01c7017aed375f7a124f1cfd372782b1cf3
luishsilva <luizhrick@gmail.com>
2024-02-11 08:25:20 -0700

    Fix add and remove error from state method

--- src/ClassApp/ClassApp.jsx
+++ src/ClassApp/ClassApp.jsx
@@ -13,6 +13,7 @@ export class ClassApp extends Component {
     firstName: "",
     lastName: "",
     phone: "",
+    hasInputError: []
   }
 
   setAppState = (newState) => {
@@ -21,49 +22,66 @@ export class ClassApp extends Component {
 
   onHandleSubmit = (e) => {
     e.preventDefault();
-    this.validadeForm(this.state)
+    this.validateForm(this.state)
     this.setState(
       {isFormSubmitted: true}
     );
   }
 
-  validadeForm = (formInputs) => {
-    if(formInputs != null){
+  addError = (key) => {
+    this.setState((prevState) => {
+      if (!prevState.hasInputError.includes(key)) {
+        return {
+          hasInputError: [...prevState.hasInputError, key],
+        };
+      }
+      return null;
+    });
+  };
+
+  removeError = (key) => {
+    this.setState((prevState) => {
+      const filteredErrors = prevState.hasInputError.filter((item) => item !== key);
+      if (filteredErrors.length !== prevState.hasInputError.length) {
+        return {
+          hasInputError: filteredErrors,
+        };
+      }
+      return null;
+    });
+  };
+
+  validateForm = (formInputs) => {
+    if (formInputs && typeof formInputs === 'object') {
       Object.entries(formInputs).forEach(([key, value]) => {
         switch (key) {
           case 'firstName':
-            if (isInputLenghtValid(value,2)) {
-              console.log('first name valid')
-            } else {
-              console.log('first name not valid')
-            }
-            break;
           case 'lastName':
-            if (isInputLenghtValid(value,2)) {
-              console.log('last name valid')
+            if (isInputLenghtValid(value, 2)) {
+              this.removeError(key);
             } else {
-              console.log('last name not valid')
+              this.addError(key);
             }
             break;
           case 'email':
             if (isEmailValid(value)) {
-              console.log('email valid')
+              this.removeError(key);
             } else {
-              console.log('email not valid')
+              this.addError(key);
             }
             break;
           case 'city':
             if (isCityValid(allCities, value)) {
-              console.log('City valid')
+              this.removeError(key);
             } else {
-              console.log('City not valid')
+              this.addError(key);
             }
             break;
           case 'phone':
             if (isPhoneValid(value)) {
-              console.log('Phone valid')
+              this.removeError(key);
             } else {
-              console.log('Phone not valid')
+              this.addError(key);
             }
             break;
           default:
@@ -73,10 +91,56 @@ export class ClassApp extends Component {
     }
   }
 
+  /* validadeForm = (formInputs) => {
+    if(formInputs != null){
+      Object.entries(formInputs).forEach(([key, value]) => {
+        switch (key) {
+          case 'firstName':
+            if (isInputLenghtValid(value,2)) {
+              this.removeErroFromState(key);
+            } else {
+              this.hasError(key);
+            }
+            break;
+          case 'lastName':
+            if (isInputLenghtValid(value,2)) {
+              this.removeErroFromState(key);
+            } else {
+              this.hasError(key);
+            }
+            break;
+          case 'email':
+            if (isEmailValid(value)) {
+              this.removeErroFromState(key);
+            } else {
+              this.hasError(key);
+            }
+            break;
+          case 'city':
+            if (isCityValid(allCities, value)) {
+              this.removeErroFromState(key);
+            } else {
+              this.hasError(key);
+            }
+            break;
+          case 'phone':
+            if (isPhoneValid(value)) {
+              this.removeErroFromState(key);
+            } else {
+              this.hasError(key);
+            }
+            break;
+          default:
+            break;
+        }
+      });
+    }
+  } */
+
   render() {
-    const { email, firstName, lastName, phone, city, } = this.state;
+    const { email, firstName, lastName, phone, city, hasInputError } = this.state;
     const { isFormSubmitted } = this.state;
-    
+
     return (
       <>
         <h2>Class</h2>
