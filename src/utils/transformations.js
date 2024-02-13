505a9793c7da9d73036ce90adebb6659d3f1ddd8
luishsilva <luizhrick@gmail.com>
2024-02-12 22:18:48 -0700

    Add formatNumber method

--- src/utils/transformations.js
+++ src/utils/transformations.js
@@ -3,7 +3,9 @@ export const capitalize = () => {
     // `capitalize("jOn")` should output `"Jon"`
 }
 
-export const formatPhoneNumber = () => {
+export const formatPhoneNumber = (value) => {
     // todo: build this function
     // `formatPhoneNumber("1234567")` should be `"12-34-56-7"`
+    const regex = /^(\d{2})(\d{2})(\d{2})(\d{1})$/;
+    return value.replace(regex, '$1-$2-$3-$4');
 }
