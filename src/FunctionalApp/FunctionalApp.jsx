d7c013126e7be062c93dda2c5819891286873f5b
luishsilva <luizhrick@gmail.com>
2024-02-16 06:29:40 -0700

    Add state variables

--- src/FunctionalApp/FunctionalApp.jsx
+++ src/FunctionalApp/FunctionalApp.jsx
@@ -1,12 +1,24 @@
+import React, { useState } from "react";
 import { ProfileInformation } from "../ProfileInformation";
 import { FunctionalForm } from "./FunctionalForm";
 
 export const FunctionalApp = () => {
+  const [state, setState] = useState({
+    isFormSubmitted: false,
+    city: '',
+    email: '',
+    firstName: '',
+    lastName: '',
+    phone: '',
+    hasInputError: [],
+    showProfileInformation: false,
+  });
+
   return (
     <>
       <h2>Functional</h2>
       <ProfileInformation userData={null} />
-      <FunctionalForm />
+      <FunctionalForm state={state} setState={setState} />
     </>
   );
 };
